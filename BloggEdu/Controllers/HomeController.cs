15cc340d807c93ad70abdb6bb50112a23a0feb68
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-02-21 18:06:27 +0100

    Last operation Migration

--- BloggEdu/Controllers/HomeController.cs
+++ BloggEdu/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace BloggEdu.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
