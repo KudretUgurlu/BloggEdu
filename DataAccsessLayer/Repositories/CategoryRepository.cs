bfd54fdb213b5b863829f171e5b4c189dfb4aeec
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-02-27 17:37:39 +0100

    Listing Categories, Category Index View

--- DataAccsessLayer/Repositories/CategoryRepository.cs
+++ DataAccsessLayer/Repositories/CategoryRepository.cs
@@ -18,6 +18,11 @@ namespace DataAccsessLayer.Repositories
             c.SaveChanges();
         }
 
+        public void Delete(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
         public void DeleteCategory(Category category)
         {
             c.Remove(category);
@@ -29,9 +34,24 @@ namespace DataAccsessLayer.Repositories
             return c.Categories.Find(id);
         }
 
+        public List<Category> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Category> ListAllCategory()
         {
             return c.Categories.ToList();
+        }
+
+        public void Update(Category t)
+        {
+            throw new NotImplementedException();
         }
 
         public void UpdateCategory(Category category)
