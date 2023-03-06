b1db8fa56774b380adc1c0a6ebe94484ae6c1ac5
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-06 18:05:45 +0100

    Listing of Blogs

--- BloggEdu/Controllers/BlogController.cs
+++ BloggEdu/Controllers/BlogController.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BloggEdu.Controllers
 {
     public class BlogController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRebository());
         public IActionResult Index()
         {
-            return View();
+            var values = bm.GetList();
+            return View(values);
         }
     }
 }
---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccsessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class BlogManager : IBlogService
+    {
+        IBlogDal _blogdal;
+
+        public BlogManager(IBlogDal blogdal)
+        {
+            _blogdal = blogdal;
+        }
+
+        public void BlogAdd(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogDelete(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void BlogUpdate(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Blog GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Blog> GetList()
+        {
+            return _blogdal.GetListAll();
+        }
+    }
+}
