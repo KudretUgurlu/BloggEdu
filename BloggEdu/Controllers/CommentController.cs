69b77ec926bce6fdce5973685b523e3af6d3f84f
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-12 00:04:54 +0100

    #29 Writing Methods for Comments(There is an error)

--- BloggEdu/Controllers/CommentController.cs
+++ BloggEdu/Controllers/CommentController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BloggEdu.Controllers
 {
     public class CommentController : Controller
     {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
         public IActionResult Index()
         {
             return View();
@@ -12,9 +15,10 @@ namespace BloggEdu.Controllers
         {
             return PartialView();
         }
-        public PartialViewResult CommentListByBlog()
+        public PartialViewResult CommentListByBlog(int id)
         {
-            return PartialView();
+            var values = cm.GetList(id);
+            return PartialView(values);
         }
     }
 }
