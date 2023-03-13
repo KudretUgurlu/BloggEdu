0c17ef9332315ab9cf1fa4e8fed97d8684553e61
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-14 00:41:39 +0100

    #32 Using Parameters with ViewComponent

--- BloggEdu/ViewComponents/Comment/CommentListByBlog.cs
+++ BloggEdu/ViewComponents/Comment/CommentListByBlog.cs
@@ -7,9 +7,9 @@ namespace BloggEdu.ViewComponents.Comment
     public class CommentListByBlog : ViewComponent
     {
         CommentManager cm = new CommentManager(new EfCommentRepository());
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(int id)
         {
-            var values = cm.GetList(4);
+            var values = cm.GetList(id);
             return View(values);
         }
     }
