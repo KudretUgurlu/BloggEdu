0a762828c48e9d34736704e7fb24d1c55c4fc503
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-13 15:02:45 +0100

    #30 Debuging (View Component) (Static)

---
+++ BloggEdu/Models/UserComment.cs
@@ -0,0 +1,8 @@
+﻿namespace BloggEdu.Models
+{
+    public class UserComment
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ BloggEdu/ViewComponents/CommentList.cs
@@ -0,0 +1,32 @@
+﻿using BloggEdu.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BloggEdu.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>()
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    UserName = "Murat",
+                },
+                new UserComment 
+                {
+                ID=2,
+                UserName="Mesut"
+                },
+                new UserComment
+                {
+                    ID=3,
+                    UserName="Merve"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
