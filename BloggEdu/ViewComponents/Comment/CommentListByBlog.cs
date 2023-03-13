8143fa0399d0734a9ab65a1416ae3a7eeabb6660
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-14 00:15:32 +0100

    #31 Listing comments for blogs

---
+++ BloggEdu/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(4);
+            return View(values);
+        }
+    }
+}
