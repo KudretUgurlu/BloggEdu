65e669aa51e21c94710dfe50092aa128ec2d2f9f
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-15 00:23:16 +0100

    #35 Fluent Validation Core

--- BloggEdu/Controllers/RegisterController.cs
+++ BloggEdu/Controllers/RegisterController.cs
@@ -1,25 +1,41 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccsessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BloggEdu.Controllers
 {
     public class RegisterController : Controller
     {
-        WriterManager wm=new WriterManager(new EfWriterRepository());
+        WriterManager wm = new WriterManager(new EfWriterRepository());
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
         [HttpPost]
-		public IActionResult Index(Writer p)
-		{
-            p.WriterStatus = true;
-            p.WriterAbout = "Deneme Test";
-            wm.WriterAdd(p);
-			return RedirectToAction("Index","Blog");
-		}
-	}
+        public IActionResult Index(Writer p)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult result = wv.Validate(p);
+            if (result.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Deneme Test";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+        }
+    }
 }
