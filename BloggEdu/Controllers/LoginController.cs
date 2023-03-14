36e98ecec0a4e102f195401defb9096f5883b2c5
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-15 00:44:39 +0100

    #36 Preparation of the login panel, class assignments on the View page

---
+++ BloggEdu/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
