062a5fe75ee825bc75fb1c6a26b08ad0ca4ffac7
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-14 01:09:39 +0100

    #33 Httpget & Httppost attributes and register operation

---
+++ BloggEdu/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
