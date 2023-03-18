2946e6f0d6680d1b5daac8fcbee8ecbddd417462
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-18 17:27:45 +0100

    #41 About Transactions, Sosial Media, Last Post (Hatalı)

---
+++ BloggEdu/Controllers/AboutController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Controllers
+{
+	public class AboutController : Controller
+	{
+		AboutManager abm=new AboutManager(new EfAboutRepository());
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public PartialViewResult SocialMediaAbout()
+		{
+			var values = abm.GetList();
+			return PartialView(values);
+		}
+	}
+}
---
+++ BusinessLayer/Concrete/AboutManager.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Abstract;
+using DataAccsessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AboutManager : IAboutService
+    {
+        IAboutDal _aboudDal;
+
+        public AboutManager(IAboutDal aboudDal)
+        {
+            _aboudDal = aboudDal;
+        }
+
+        public List<About> GetList()
+        {
+            return _aboudDal.GetListAll();
+        }
+    }
+}
