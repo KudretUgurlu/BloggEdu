f719c6100f1460cbf16200a47734b1a5c977a1e3
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-18 18:17:14 +0100

    #41.1 SocialMediaAbout Problem Not Solved (Unable to fetch with the await method, it disappears.)

--- BloggEdu/Controllers/AboutController.cs
+++ BloggEdu/Controllers/AboutController.cs
@@ -4,17 +4,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BloggEdu.Controllers
 {
-	public class AboutController : Controller
-	{
-		AboutManager abm=new AboutManager(new EfAboutRepository());
-		public IActionResult Index()
-		{
-			return View();
-		}
-		public PartialViewResult SocialMediaAbout()
-		{
-			var values = abm.GetList();
-			return PartialView(values);
-		}
-	}
+    public class AboutController : Controller
+    {
+        AboutManager abm = new AboutManager(new EfAboutRepository());
+        public IActionResult Index()
+        {
+            var values = abm.GetList();
+            return View(values);
+        }
+        public PartialViewResult SocialMediaAbout()
+        {
+            return PartialView();
+        }
+    }
 }
