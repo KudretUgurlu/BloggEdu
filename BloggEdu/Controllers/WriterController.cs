88b8c9462f91483275a21dfdf64cac1c844adf8a
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-21 23:59:15 +0100

    #44 Autentication and Controller Level Authorize

---
+++ BloggEdu/Controllers/WriterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Controllers
+{
+    [Authorize]
+    public class WriterController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+    }
+}
