81ebfe3e7de7ea9a0283777a69b2f077ffb645b6
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-23 01:28:27 +0100

    #50 Adding an Writer Panel Theme file

--- BloggEdu/Controllers/WriterController.cs
+++ BloggEdu/Controllers/WriterController.cs
@@ -18,5 +18,10 @@ namespace BloggEdu.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
