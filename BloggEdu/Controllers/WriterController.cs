0cc689ff70baf1d978d1f0473c84fd9455e5f0de
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-23 01:56:21 +0100

    #51 Writer Navbar Edits

--- BloggEdu/Controllers/WriterController.cs
+++ BloggEdu/Controllers/WriterController.cs
@@ -23,5 +23,16 @@ namespace BloggEdu.Controllers
         {
             return View();
         }
+        [AllowAnonymous]
+        public PartialViewResult WriterNavbarPartial()
+        {
+            return PartialView();
+        }
+        [AllowAnonymous]
+        public PartialViewResult WriterFooterPartial()
+        {
+            return PartialView();
+        }
+
     }
 }
