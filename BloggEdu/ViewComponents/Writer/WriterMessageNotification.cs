cbfccefba63d70a3b6f5d55bfdb55119d8b2f5cb
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-23 02:13:34 +0100

    #52 Writer Top Menu View Components

---
+++ BloggEdu/ViewComponents/Writer/WriterMessageNotification.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.ViewComponents.Writer
+{
+    public class WriterMessageNotification:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ BloggEdu/ViewComponents/Writer/WriterNotification.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.ViewComponents.Writer
+{
+    public class WriterNotification:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
