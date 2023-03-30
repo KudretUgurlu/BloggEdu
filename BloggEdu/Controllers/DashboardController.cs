5957eedd37154fe36af1a637e1907cc61d1ba113
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-03-30 15:22:14 +0200

    #61 Dashboard Preparation for the Writer

---
+++ BloggEdu/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
