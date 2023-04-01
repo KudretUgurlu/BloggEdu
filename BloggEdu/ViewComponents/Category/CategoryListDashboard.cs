b9420751c0e1415f169787fc313b9c7311dc06fa
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-01 02:03:56 +0200

    #63 Dashboard Preparation-2 for the Writer

---
+++ BloggEdu/ViewComponents/Category/CategoryListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.ViewComponents.Category
+{
+    public class CategoryListDashboard:ViewComponent
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
