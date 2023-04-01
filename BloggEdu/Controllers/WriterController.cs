66b1156f88efae486a560003eac8101d759639d9
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-01 17:26:48 +0200

    #73 Uploading Profile Photo from File

--- BloggEdu/Controllers/WriterController.cs
+++ BloggEdu/Controllers/WriterController.cs
@@ -1,10 +1,13 @@
-﻿using BusinessLayer.Concrete;
+﻿using BloggEdu.Models;
+using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
 using DataAccsessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 
 namespace BloggEdu.Controllers
 {
@@ -65,6 +68,33 @@ namespace BloggEdu.Controllers
             }
             return View();
         }
-
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+            Writer w = new Writer();
+            if (p.WriterImage != null)
+            {
+                var extension = Path.GetExtension(p.WriterImage.FileName);
+                var newimagename = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/", newimagename);
+                var stream = new FileStream(location, FileMode.Create);
+                p.WriterImage.CopyTo(stream);
+                w.WriterImage = newimagename;
+            }
+            w.WriterMail = p.WriterMail;
+            w.WriterName = p.WriterName;
+            w.WriterPassword = p.WriterPassword;
+            w.WriterStatus = true;
+            w.WriterAbout = p.WriterAbout;
+            wm.TAdd(w);
+            return RedirectToAction("Index", "Dashboard");
+        }
     }
 }
