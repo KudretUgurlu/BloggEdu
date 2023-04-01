6a9e28f3194e6fce9d2454cb11d55217ee40bfa8
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-01 02:48:30 +0200

    #70 Dashboard Statistic (Using LinQ)

--- BloggEdu/Controllers/DashboardController.cs
+++ BloggEdu/Controllers/DashboardController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using DataAccsessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BloggEdu.Controllers
 {
@@ -8,6 +10,10 @@ namespace BloggEdu.Controllers
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c= new Context();
+            ViewBag.v1=c.Blogs.Count().ToString();
+            ViewBag.v2=c.Blogs.Where(x=>x.WriterID==1).Count();
+            ViewBag.v3=c.Categories.Count().ToString();
             return View();
         }
     }
