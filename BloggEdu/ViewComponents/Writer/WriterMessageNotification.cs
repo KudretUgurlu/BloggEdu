4864ddaffc9cdc97f9fcba7a70705c9617278585
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-01 18:59:27 +0200

    #78 Completing Message Notifications

--- BloggEdu/ViewComponents/Writer/WriterMessageNotification.cs
+++ BloggEdu/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BloggEdu.ViewComponents.Writer
 {
     public class WriterMessageNotification:ViewComponent
     {
+        MessageManager mm=new MessageManager(new EfMessageRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p;
+            p = "deneme@gmail.com";
+            var values = mm.GetInboxListByWriter(p);
+            return View(values);
         }
     }
 }
