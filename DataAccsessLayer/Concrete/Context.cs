40cd88c8d262ee71e537bd61e105b3818056030b
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-07 01:43:23 +0200

    #81 Associating Writers with Messages

--- DataAccsessLayer/Concrete/Context.cs
+++ DataAccsessLayer/Concrete/Context.cs
@@ -26,11 +26,27 @@ namespace DataAccsessLayer.Concrete
                 .WithMany(y => y.AwayMatches)
                 .HasForeignKey(z => z.GuestTeamID)
                 .OnDelete(DeleteBehavior.ClientSetNull);
-            // 14:55 te kaldım
+
+            modelBuilder.Entity<Message2>()
+                .HasOne(x => x.SenderUser)
+                .WithMany(y => y.WriterSender)
+                .HasForeignKey(z => z.SenderID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+            modelBuilder.Entity<Message2>()
+                .HasOne(x => x.ReceiverUser)
+                .WithMany(y => y.WriterReceiver)
+                .HasForeignKey(z => z.ReceiverID)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+            //HomeMatches-->WriterSender
+            //AwayMatches-->WriterReceiver
+
+            //HomeTeam-->SenderUser
+            //GuestTeam-->ReceiverUser
 
         }
 
-        public DbSet<About> Abouts { get; set; }
+        public DbSet<About> Abouts { get; set; } 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Comment> Comments { get; set; }
@@ -42,5 +58,6 @@ namespace DataAccsessLayer.Concrete
         public DbSet<Message> Messages { get; set; }
         public DbSet<Team> Teams { get; set; }
         public DbSet<Match> Matches { get; set; }
+        public DbSet<Message2> Message2s { get; set; }
     }
 }
--- DataAccsessLayer/Migrations/20230406233940_mig_messagetable_relation.Designer.cs
+++ DataAccsessLayer/Migrations/20230406233940_mig_messagetable_relation.Designer.cs
@@ -4,14 +4,16 @@ using DataAccsessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccsessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230406233940_mig_messagetable_relation")]
+    partial class mig_messagetable_relation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
