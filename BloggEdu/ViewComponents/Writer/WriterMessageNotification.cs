a651bc8d10e4421dd63a1476bdfe947ce25bdc51
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-07 02:07:34 +0200

    #82 Writer's Message Notifications

--- BloggEdu/ViewComponents/Writer/WriterMessageNotification.cs
+++ BloggEdu/ViewComponents/Writer/WriterMessageNotification.cs
@@ -6,12 +6,11 @@ namespace BloggEdu.ViewComponents.Writer
 {
     public class WriterMessageNotification:ViewComponent
     {
-        MessageManager mm=new MessageManager(new EfMessageRepository());
+        Message2Manager mm=new Message2Manager(new EfMessage2Repository());
         public IViewComponentResult Invoke()
         {
-            string p;
-            p = "deneme@gmail.com";
-            var values = mm.GetInboxListByWriter(p);
+            int id = 2;
+            var values = mm.GetInboxListByWriter(id);
             return View(values);
         }
     }
---
+++ DataAccsessLayer/EntityFramework/EfMessage2Repository.cs
@@ -0,0 +1,15 @@
+﻿using DataAccsessLayer.Abstract;
+using DataAccsessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccsessLayer.EntityFramework
+{
+    public class EfMessage2Repository : GenericRepository<Message2>, IMessage2Dal
+    {
+    }
+}
