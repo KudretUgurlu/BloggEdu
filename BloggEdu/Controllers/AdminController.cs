436273849711c607ffa57a095df264d38baab4d7
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-15 12:26:07 +0200

    #87 Admin Theme

---
+++ BloggEdu/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Controllers
+{
+    
+    public class AdminController : Controller
+    {
+      
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
