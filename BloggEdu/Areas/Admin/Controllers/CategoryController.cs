5e50fb25f1a3646c46543dd5386f52e079e38f71
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-16 01:57:43 +0200

    #91 Listing Categories in Admin Panel

--- BloggEdu/Areas/Admin/Controllers/CategoryController.cs
+++ BloggEdu/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BloggEdu.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+            return View(values);
         }
     }
 }
