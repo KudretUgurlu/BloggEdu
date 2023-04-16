aab01dfae04ea291ab0f4c80c2c48e39ebe954c8
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-16 02:08:17 +0200

    #92 Pagination

--- BloggEdu/Areas/Admin/Controllers/CategoryController.cs
+++ BloggEdu/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccsessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using X.PagedList;
 
 namespace BloggEdu.Areas.Admin.Controllers
 {
@@ -8,9 +9,9 @@ namespace BloggEdu.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         CategoryManager cm = new CategoryManager(new EfCategoryRepository());
-        public IActionResult Index()
+        public IActionResult Index(int page=1)
         {
-            var values = cm.GetList();
+            var values = cm.GetList().ToPagedList(page,3);
             return View(values);
         }
     }
