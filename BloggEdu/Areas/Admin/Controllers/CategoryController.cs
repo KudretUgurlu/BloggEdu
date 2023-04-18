26a921a8f92788a4d74cc06493d3dc6ce3f5f1d6
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-18 13:16:35 +0200

    #93 Adding a New Category in the Admin Panel

--- BloggEdu/Areas/Admin/Controllers/CategoryController.cs
+++ BloggEdu/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,10 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccsessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using X.PagedList;
 
 namespace BloggEdu.Areas.Admin.Controllers
@@ -13,6 +17,33 @@ namespace BloggEdu.Areas.Admin.Controllers
         {
             var values = cm.GetList().ToPagedList(page,3);
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory() 
+        {
+        return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p) 
+        {
+          
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult result = cv.Validate(p);
+            if (result.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
         }
     }
 }
