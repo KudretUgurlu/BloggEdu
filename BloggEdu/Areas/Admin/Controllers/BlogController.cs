8dbcd110dbb7be99ee837ebdd278966aad39934b
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-19 01:34:05 +0200

    #97 Excel Dinamic Table Export

--- BloggEdu/Areas/Admin/Controllers/BlogController.cs
+++ BloggEdu/Areas/Admin/Controllers/BlogController.cs
@@ -1,8 +1,10 @@
 ﻿using BloggEdu.Areas.Admin.Models;
 using ClosedXML.Excel;
+using DataAccsessLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace BloggEdu.Areas.Admin.Controllers
 {
@@ -24,10 +26,10 @@ namespace BloggEdu.Areas.Admin.Controllers
                     worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
                     BlogRowCount++;
                 }
-                using(var stream=new MemoryStream())
+                using (var stream = new MemoryStream())
                 {
                     workbook.SaveAs(stream);
-                    var content=stream.ToArray();
+                    var content = stream.ToArray();
                     return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
                 }
             }
@@ -43,6 +45,47 @@ namespace BloggEdu.Areas.Admin.Controllers
             return bm;
         }
         public IActionResult BlogListExcel()
+        {
+            return View();
+        }
+
+        public IActionResult ExportDynamicExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in BlogTitleList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content = stream.ToArray();
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+        }
+        public List<BlogModel2> BlogTitleList()
+        {
+            List<BlogModel2> bm = new List<BlogModel2>();
+            using (var c = new Context())
+            {
+                bm = c.Blogs.Select(x => new BlogModel2()
+                {
+                    ID = x.BlogID,
+                    BlogName = x.BlogTitle
+                }).ToList();
+            }
+            return bm;
+        }
+        public IActionResult BlogTitleListExcel()
         {
             return View();
         }
---
+++ BloggEdu/Areas/Admin/Models/BlogModel2.cs
@@ -0,0 +1,8 @@
+﻿namespace BloggEdu.Areas.Admin.Models
+{
+    public class BlogModel2
+    {
+        public int ID { get; set; }
+        public string BlogName { get; set; }
+    }
+}
