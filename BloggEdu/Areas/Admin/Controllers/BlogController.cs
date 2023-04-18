6d2cd69f862570bac80cfa8a756026644660d07a
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-19 01:09:40 +0200

    #95 Exel Export - 1

---
+++ BloggEdu/Areas/Admin/Controllers/BlogController.cs
@@ -0,0 +1,45 @@
+﻿using BloggEdu.Areas.Admin.Models;
+using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BloggEdu.Areas.Admin.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult ExportStaticExcelBlogList()
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Blog Listesi");
+                worksheet.Cell(1, 1).Value = "Blog ID";
+                worksheet.Cell(1, 2).Value = "Blog Adı";
+
+                int BlogRowCount = 2;
+                foreach (var item in GetBlogList())
+                {
+                    worksheet.Cell(BlogRowCount, 1).Value = item.ID;
+                    worksheet.Cell(BlogRowCount, 2).Value = item.BlogName;
+                    BlogRowCount++;
+                }
+                using(var stream=new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+                    var content=stream.ToArray();
+                    return File(content, "application / vnd.openxmlformats - officedocument.spreadsheetml.sheet", "Calisma1.xlsx");
+                }
+            }
+        }
+        public List<BlogModel> GetBlogList()
+        {
+            List<BlogModel> bm = new List<BlogModel>
+            {
+               new BlogModel {ID=1,BlogName="C# Programlamaya Giriş"},
+               new BlogModel {ID=2,BlogName="Tesla Firmasının Araçları"},
+               new BlogModel {ID=3,BlogName="2020 Olimpiyatları"}
+            };
+            return bm;
+        }
+    }
+}
---
+++ BloggEdu/Areas/Admin/Models/BlogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BloggEdu.Areas.Admin.Models
+{
+    public class BlogModel
+    {
+        public int ID { get; set; }
+        public string BlogName { get; set; }
+    }
+}
