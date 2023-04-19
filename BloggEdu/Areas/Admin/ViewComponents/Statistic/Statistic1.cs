75c0e24bbcf9fc94e7cf49572162639eb4fa5976
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-19 02:18:00 +0200

    #99 Statistics

---
+++ BloggEdu/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BloggEdu.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRebository());
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.GetList().Count();
+            ViewBag.v2=c.Contacts.Count();
+            ViewBag.v3=c.Comments.Count();
+            return View();
+        }
+    }
+}
