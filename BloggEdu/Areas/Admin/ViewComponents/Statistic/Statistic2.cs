296e051899128562b3209126649ea82cc63dd897
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-19 02:37:18 +0200

    #100 Statistic 2

---
+++ BloggEdu/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,17 @@
+﻿using DataAccsessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BloggEdu.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Blogs.OrderByDescending(x=>x.BlogID).Select(x=>x.BlogTitle).Take(1).FirstOrDefault();
+            ViewBag.v3 = c.Comments.Count();
+            return View();
+        }
+    }
+}
