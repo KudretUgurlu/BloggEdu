bb7f1ec04df4ebc0c543722fc0ce572499bb27d9
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-04-19 02:00:24 +0200

    #98 Widgets

---
+++ BloggEdu/Areas/Admin/Controllers/WidgetController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Areas.Admin.Controllers
+{
+    public class WidgetController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
