9f76a2001f852da60c54eea0f935825439769c78
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-05-10 16:03:08 +0200

    #102 Statistic 3

--- BloggEdu/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
+++ BloggEdu/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccsessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BloggEdu.Areas.Admin.ViewComponents.Statistic
 {
     public class Statistic4 : ViewComponent
     {
+        Context c =new Context(); 
         public IViewComponentResult Invoke()
         {
+            ViewBag.v1 = c.Admins.Where(x => x.AdminID == 1).Select(y => y.Name).FirstOrDefault();
+            ViewBag.v2= c.Admins.Where(x => x.AdminID == 1).Select(y => y.ImageUrl).FirstOrDefault();
+            ViewBag.v3= c.Admins.Where(x => x.AdminID == 1).Select(y => y.ShortDescription).FirstOrDefault();
             return View(); 
         }
     }
---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccsessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager:IAdminService
+    {
+        IAdminDal _Admindal;
+
+        public AdminManager(IAdminDal admindal)
+        {
+            _Admindal = admindal;
+        }
+
+        public List<Admin> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admin TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Admin t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
