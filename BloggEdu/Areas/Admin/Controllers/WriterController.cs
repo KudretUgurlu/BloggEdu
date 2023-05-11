6168f41c4843710c39a3f5d7f29e0b842771af45
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-05-12 00:41:57 +0200

    #106 Console Listing of Data with Ajax

---
+++ BloggEdu/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,42 @@
+﻿using BloggEdu.Areas.Admin.Models;
+using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace BloggEdu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass
+            {
+            Id = 1,
+            Name="Ayşe"
+            },
+            new WriterClass
+            {
+            Id = 2,
+            Name="Ahmet"
+            },
+            new WriterClass
+            {
+            Id = 3,
+            Name="Buse"
+            }
+        };
+    }
+}
---
+++ BloggEdu/Areas/Admin/Models/WriterClass.cs
@@ -0,0 +1,8 @@
+﻿namespace BloggEdu.Areas.Admin.Models
+{
+    public class WriterClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
