06a355bb77901f79c5c1e41227b9d36c5b4d1fac
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-05-14 00:44:48 +0200

    #114 Introduction to API

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=BloggEduApiDB;Integrated Security=True;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
