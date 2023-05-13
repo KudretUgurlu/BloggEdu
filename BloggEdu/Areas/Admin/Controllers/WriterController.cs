0694ae3b8fa42d7c91cd01e529cd58bb32ad9f9b
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-05-14 00:06:49 +0200

    #112 Deleting with Ajax

--- BloggEdu/Areas/Admin/Controllers/WriterController.cs
+++ BloggEdu/Areas/Admin/Controllers/WriterController.cs
@@ -36,6 +36,13 @@ namespace BloggEdu.Areas.Admin.Controllers
             return Json(jsonWriters);
         }
 
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
