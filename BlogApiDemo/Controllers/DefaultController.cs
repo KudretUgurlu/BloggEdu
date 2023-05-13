ec9bf830d3667c7cd80a8ae21408813259332789
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-05-14 00:50:13 +0200

    #115 Swagger and Api Get List

---
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using BlogApiDemo.DataAccessLayer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BlogApiDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult EmployeeList()
+        {
+            using var c = new Context();
+            var values=c.Employees.ToList();
+            return Ok(values);
+        }
+    }
+}
