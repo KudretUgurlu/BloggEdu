6c3d32322c5dc78146356fc6aab28484ff099ff8
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-05-14 00:23:23 +0200

    #113 Update with Ajax

--- BloggEdu/Areas/Admin/Controllers/WriterController.cs
+++ BloggEdu/Areas/Admin/Controllers/WriterController.cs
@@ -43,6 +43,14 @@ namespace BloggEdu.Areas.Admin.Controllers
             return Json(writer);
         }
 
+        public IActionResult UpdateWriter(WriterClass w)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == w.Id);
+            writer.Name = w.Name;
+            var jsonWriter = JsonConvert.SerializeObject(w);
+            return Json(jsonWriter);
+        }
+
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
