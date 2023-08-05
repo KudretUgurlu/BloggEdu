8bd5d39232ea9ab7e3819a0aaf9459f64d3df518
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-08-06 00:22:42 +0200

    #136 Admin Panel Outbox and New Message Page

--- BloggEdu/Areas/Admin/Controllers/AdminMessageController.cs
+++ BloggEdu/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,11 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace BloggEdu.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminMessageController : Controller
     {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
         public IActionResult Inbox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListByWriter(writerID);
+            return View(values);
+        }
+        public IActionResult SendBox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetSendBoxListByWriter(writerID);
+            return View(values);
+        }
+        public IActionResult ComposeMessage()
         {
             return View();
         }
