944d31cb97be38d7d8035608219588c1d87975b0
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-08-06 16:37:55 +0200

    #137 Admin Blog Lists

---
+++ BloggEdu/Areas/Admin/Controllers/AdminBlogController.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccsessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggEdu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminBlogController : Controller
+    {
+        BlogManager blogManager = new BlogManager(new EfBlogRebository());
+        public IActionResult Index()
+        {
+            var values = blogManager.GetBlogListWithCategory();
+            return View(values);
+        }
+    }
+}
--- BloggEdu/Areas/Admin/Controllers/AdminMessageController.cs
+++ BloggEdu/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,7 +1,9 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccsessLayer.Concrete;
 using DataAccsessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace BloggEdu.Areas.Admin.Controllers
@@ -27,9 +29,23 @@ namespace BloggEdu.Areas.Admin.Controllers
             var values = mm.GetSendBoxListByWriter(writerID);
             return View(values);
         }
+        [HttpGet]
         public IActionResult ComposeMessage()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            p.SenderID= writerID;
+            p.ReceiverID = 2;
+            p.MessageDate=Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.MessageStatus = true;
+            mm.TAdd(p);
+            return RedirectToAction("SendBox");
         }
     }
 }
