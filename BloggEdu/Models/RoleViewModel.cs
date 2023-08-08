907ea81a2ca5fc45739207ab2093454418899a4a
Kudret Ugurlu Gumusoz <119495519+KudretUgurlu@users.noreply.github.com>
2023-08-08 20:29:04 +0200

    #139 Role List and Add Roles

---
+++ BloggEdu/Models/RoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggEdu.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage ="Lütfen rol adı giriniz")]
+        public string name { get; set; }
+
+    }
+}
