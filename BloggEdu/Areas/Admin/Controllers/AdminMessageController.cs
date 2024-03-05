62071f6f67299ca07b560fd88e0a9fcd08b6b0ef
kudret ugurlu Gumusoz <kudretgumusoz@gmail.com>
2024-03-05 14:26:44 +0100

    #176 ReceiverID was made dynamic. Added the number of messages in the Outbox and Inbox.

--- BloggEdu/Areas/Admin/Controllers/AdminMessageController.cs
+++ BloggEdu/Areas/Admin/Controllers/AdminMessageController.cs
@@ -19,6 +19,10 @@ namespace BloggEdu.Areas.Admin.Controllers
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = mm.GetInboxListByWriter(writerID);
+            var Inboxcount = mm.GetInboxListByWriter(writerID).Count();
+            ViewBag.Inboxcount = Inboxcount;
+            var Senboxcount = mm.GetSendBoxListByWriter(writerID).Count();
+            ViewBag.Senboxcount = Senboxcount;
             return View(values);
         }
         public IActionResult SendBox()
@@ -27,6 +31,10 @@ namespace BloggEdu.Areas.Admin.Controllers
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
             var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = mm.GetSendBoxListByWriter(writerID);
+            var Inboxcount = mm.GetInboxListByWriter(writerID).Count();
+            ViewBag.Inboxcount = Inboxcount;
+            var Senboxcount = mm.GetSendBoxListByWriter(writerID).Count();
+            ViewBag.Senboxcount = Senboxcount;
             return View(values);
         }
         [HttpGet]
@@ -35,17 +43,26 @@ namespace BloggEdu.Areas.Admin.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult ComposeMessage(Message2 p)
+        public IActionResult ComposeMessage(Message2 model, string ReceiverEmail)
         {
             var username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
-            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
-            p.SenderID= writerID;
-            p.ReceiverID = 2;
-            p.MessageDate=Convert.ToDateTime(DateTime.Now.ToShortDateString());
-            p.MessageStatus = true;
-            mm.TAdd(p);
+            var senderID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+
+            var receiverID = c.Users.Where(x => x.Email == ReceiverEmail).Select(y => y.Id).FirstOrDefault();
+            if (receiverID == 0)
+            {
+                ViewBag.ErrorMessage = "Alıcı bulunamadı.";
+                return View(model);
+            }
+            model.SenderID = senderID;
+            model.ReceiverID = receiverID;
+            model.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            model.MessageStatus = true;
+            mm.TAdd(model);
+
             return RedirectToAction("SendBox");
         }
+
     }
 }
--- BloggEdu/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ BloggEdu/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -14,13 +14,13 @@ namespace BloggEdu.Areas.Admin.ViewComponents.Statistic
         public IViewComponentResult Invoke()
         {
             ViewBag.v1 = bm.GetList().Count();
-            ViewBag.v2=c.Contacts.Count();
-            ViewBag.v3=c.Comments.Count();
+            ViewBag.v2 = c.Contacts.Count();
+            ViewBag.v3 = c.Comments.Count();
 
             string api = "f78fb14cfbd5388a8af5227b7d1b01ca";
             string connection = "https://api.openweathermap.org/data/2.5/weather?q=istanbul&mode=xml&lang=tr&units=metric&appid=" + api;
             XDocument document = XDocument.Load(connection);
-            ViewBag.v4=document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
             return View();
         }
     }
